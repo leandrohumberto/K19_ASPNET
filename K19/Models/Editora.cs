6052010978f7b2881b5c45440fd4a2cf8893783b
Leandro Humberto <lehumberto@msn.com>
2018-01-27 21:27:02 -0200

    Capítulo 04 - Visão Geral do ASP.NET MVC.

---
+++ K19/Models/Editora.cs
@@ -0,0 +1,9 @@
+﻿namespace K19.Models
+{
+    public class Editora
+    {
+        public int EditoraID { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ K19/Models/Livro.cs
@@ -0,0 +1,11 @@
+﻿namespace K19.Models
+{
+    public class Livro
+    {
+        public int LivroID { get; set; }
+        public string Titulo { get; set; }
+        public double Preco { get; set; }
+        public int EditoraID { get; set; }
+        public virtual Editora Editora { get; set; }
+    }
+}
