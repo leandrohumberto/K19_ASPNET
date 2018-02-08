65520e099f1b7247c5e3a73e6409b6d185a6d93c
Leandro Humberto <lehumberto@msn.com>
2018-02-08 17:06:57 -0200

    Capítulo 10 - Tratamento de Erros.

---
+++ TratamentoDeErros/Controllers/TesteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TratamentoDeErros.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult TestaErro()
+        {
+            string[] nomes = { "Leandro", "Humberto" };
+            string nome = nomes[2];
+            return View();
+        }
+    }
+}
---
+++ TratamentoDeErros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TratamentoDeErros.MvcApplication" Language="C#" %>
